e243591b36c9d67ef40d13485d5ecc7968f3c1d0
yashpo <yash.ponakala@gmail.com>
2020-05-03 20:04:24 -0700

    initial commit

---
+++ src/main/java/com/sb/chat/WebsocketChatApplication.java
@@ -0,0 +1,12 @@
+package com.sb.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsocketChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsocketChatApplication.class, args);
+	}
+}
---
+++ src/main/java/com/sb/chat/controller/WebSocketChatController.java
@@ -0,0 +1,28 @@
+package com.sb.chat.controller;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.stereotype.Controller;
+
+import com.sb.chat.domain.WebSocketChatMessage;
+
+@Controller
+public class WebSocketChatController {
+
+	@MessageMapping("/chat.sendMessage")
+	@SendTo("/topic/javainuse")
+	public WebSocketChatMessage sendMessage(@Payload WebSocketChatMessage webSocketChatMessage) {
+		return webSocketChatMessage;
+	}
+
+	@MessageMapping("/chat.newUser")
+	@SendTo("/topic/javainuse")
+	public WebSocketChatMessage addUser(@Payload WebSocketChatMessage webSocketChatMessage,
+			SimpMessageHeaderAccessor headerAccessor) {
+		headerAccessor.getSessionAttributes().put("username", webSocketChatMessage.getSender());
+		return webSocketChatMessage;
+	}
+
+}
